a1d2144cff806706d5dde7556678923b43a4a0ab
bhupendra771 <98879600+bhupendra771@users.noreply.github.com>
2022-03-26 20:08:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# from fastapi import FastAPI
+
+# from enum import Enum
+
+# app = FastAPI()
+
+# food_items = {
+#     'indian' : ["samosa", "Dosa"],
+#     'american' : ["Hot Dog","Apple Pie"],
+#     'italian': [ "Ravioli", "Pizza"]
+# }
+
+# @app.get("/get_items/{cuisine}")
+# async def get_items(cuisine):
+#     return food_items.get(cuisine)
+print(5*5)
